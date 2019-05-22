087a2fedbe169793334705e05da7117d9e8adab8
Diego Pasquali <imdiegopasquali@gmail.com>
2019-05-22 15:27:08 +0200

    Stored token

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import Authenticator from './lib/Authenticator';
-import * as decode from 'jwt-decode';
 
 let statusBarItem: vscode.StatusBarItem;
 
@@ -9,15 +8,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// Create the login command
 	const loginCommand = 'extension.login';
 	context.subscriptions.push(vscode.commands.registerCommand(loginCommand, () => {
+		if (context.globalState.get('token')) {
+			return;
+		}
+
 		new Authenticator()
 			.start()
-			.then((token) => vscode.window.showInformationMessage(token));
+			.then((token) => {
+				context.globalState.update('token', token);
+				statusBarItem.text = 'Codcut';
+			});
 	}));
 
 	// Create the status bar item
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 	statusBarItem.command = loginCommand;
-	statusBarItem.text = 'Codcut';
+	statusBarItem.text = context.globalState.get('token') ? 'Codcut' : 'Codcut - login';
 	context.subscriptions.push(statusBarItem);
 	statusBarItem.show();
 }
