e551772ea99260530ec9d46cd86970619ee7fd6f
Diego Pasquali <imdiegopasquali@gmail.com>
2019-05-24 11:38:59 +0200

    Abstracted ShareInput

---
+++ src/ShareInput.ts
@@ -0,0 +1,76 @@
+import * as vscode from 'vscode';
+import fetch from 'node-fetch';
+
+interface IShareInputParams {
+  code: string;
+  language: string;
+  body: string;
+}
+
+class ShareInput {
+  MAX_DESCRIPTION_LENGTH = 500;
+  MIN_DESCRIPTION_LENGTH = 5;
+
+  params: IShareInputParams = {
+    code: '',
+    language: '',
+    body: ''
+  };
+
+  token: string;
+  
+  constructor(params: Partial<IShareInputParams>, token: string) {
+    this.params = {
+      ...this.params,
+      ...params
+    };
+
+    this.token = token;
+  }
+
+  open() {
+    const { MAX_DESCRIPTION_LENGTH, MIN_DESCRIPTION_LENGTH, params } = this;
+
+    if (params.code.length < MIN_DESCRIPTION_LENGTH) {
+			return;
+		}
+
+    vscode.window.showInputBox({
+			placeHolder: `Enter a description (max ${MAX_DESCRIPTION_LENGTH} characters)`,
+			prompt: 'Share this selection on Codcut',
+
+			validateInput: this._validateDescription
+		})
+			.then((value) => {
+				if (value === undefined) {
+					return;
+				}
+
+        params.body = value;
+        
+        this._share();
+			});
+  }
+
+  _validateDescription(value: string) {
+    const { MAX_DESCRIPTION_LENGTH } = this;
+
+    if (value.length > MAX_DESCRIPTION_LENGTH) {
+      return `The description is too long (+${value.length - MAX_DESCRIPTION_LENGTH} characters)`;
+    }
+
+    return null;
+  }
+
+  _share() {
+    const { params } = this;
+    
+    if (!params.code || !params.language || !params.body) {
+      return;
+    }
+
+    console.log(params);
+  }
+}
+
+export default ShareInput;
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,17 @@
 import * as vscode from 'vscode';
 import Authenticator from './lib/Authenticator';
 import StatusBarLabel from './StatusBarLabel';
+import ShareInput from './ShareInput';
 
 const SBLabel = new StatusBarLabel();
 
 export function activate(context: vscode.ExtensionContext) {
 	const { globalState } = context;
 	
-	// Create the login command
+	// Handle the login command
 	const loginCommand = 'extension.login';
 	context.subscriptions.push(vscode.commands.registerCommand(loginCommand, () => {
-		if (globalState.get('token')) {
+		if (globalState.get<string>('token')) {
 			return;
 		}
 
@@ -20,6 +21,27 @@ export function activate(context: vscode.ExtensionContext) {
 				context.globalState.update('token', token);
 				SBLabel.setText('Codcut');
 			});
+	}));
+
+	// Handle the share command
+	const shareCommand = 'extension.share';
+	context.subscriptions.push(vscode.commands.registerCommand(shareCommand, () => {
+		const editor = vscode.window.activeTextEditor;
+		const token = globalState.get<string>('token');
+		
+		if (!editor || !token) {
+			return;
+		}
+
+		const partialParams = {
+			code: editor.document.getText(editor.selection),
+
+			// FIXME: should handle this
+			language: editor.document.fileName.split('.').pop()
+		};
+
+		new ShareInput(partialParams, token)
+			.open();
 	}));
 
 	// Create the status bar item
