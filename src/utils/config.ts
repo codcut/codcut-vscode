494e54b4894d2056064375c0a70fe71f4e052833
Diego Pasquali <diego.pasquali@efficiam.com>
2019-07-01 22:28:51 +0000

    Updated resource host

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
-export const RESOURCE_HOST = 'https://resource.codcut.com';
+export const RESOURCE_HOST = 'https://resource.codcut.com/api';
 export const POST_PREVIEW_ENDPOINT = 'https://codcut.com/posts';
